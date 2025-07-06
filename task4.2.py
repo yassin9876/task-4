bf3ef45afcf18d99c4a82a85fcd3eba64d06e08b
yassin9876 <yassin24.tamer09@gmail.com>
2025-07-06 12:43:01 +0300

    Add files via upload

---
+++ task4.2.py
@@ -0,0 +1,14 @@
+def calculator(a,b, operation):
+    if operation == "add":
+        return a + b
+    elif operation == "subtract":
+        return a - b
+    elif operation == "multiply":
+        return a * b
+    elif operation == "divide":
+        if b != 0:
+            return a/b
+        else: return "can't divide " 
+    
+    
+    
